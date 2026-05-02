710350ecbd59fdd36bfe7a23e286bd1a5d566b4e
JhontanAlvarado <alvaj802@gmail.com>
2026-05-02 01:52:28 -0400

    feat: add Paciente model in models.py

--- clinicaDjango/appClinicaDjango/models.py
+++ clinicaDjango/appClinicaDjango/models.py
@@ -12,3 +12,10 @@ class Habilidad(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return self.nombre
