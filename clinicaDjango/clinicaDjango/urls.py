e678747fb2d11ad4254bad7973c3514ad74735ff
Andre <173570505+znolly@users.noreply.github.com>
2026-05-02 06:51:51 -0400

    fix: correct main routing and initialize app views

--- clinicaDjango/clinicaDjango/urls.py
+++ clinicaDjango/clinicaDjango/urls.py
@@ -1,11 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index_especialidades, name='index'),
-    path('especialidades/<int:especialidad_id>/', views.show_especialidad, name='detail'),
-    path('especialidades/<int:especialidad_id>/doctores', views.index_doctores, name='doctores'),
-    path('doctores/<int:doctor_id>', views.show_doctor, name='doctor'),
-    path('habilidades/<int:habilidad_id>', views.show_habilidad, name='habilidad'),
-    path('pacientes/<int:paciente_id>', views.show_paciente, name='paciente'),
+    path('admin/', admin.site.urls),
+    path('appClinicaDjango/', include('appClinicaDjango.urls')),
 ]
