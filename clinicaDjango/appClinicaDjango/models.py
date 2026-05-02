ce2afaf320019274e9818f69988e1fa5af476e9b
JhontanAlvarado <alvaj802@gmail.com>
2026-05-02 01:38:02 -0400

    feat: add Especialidad model with nombre and telefono fields

--- clinicaDjango/appClinicaDjango/models.py
+++ clinicaDjango/appClinicaDjango/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Especialidad(models.Model):
+    nombre = models.CharField(max_length=50)
+    telefono = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
