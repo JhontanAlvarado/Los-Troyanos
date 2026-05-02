9422d8dcc31fd6c2fbc93a0538b37234facd3c6c
JhontanAlvarado <alvaj802@gmail.com>
2026-05-02 01:44:53 -0400

    feat: add Habilidad model with nombre field in models.py

--- clinicaDjango/appClinicaDjango/models.py
+++ clinicaDjango/appClinicaDjango/models.py
@@ -7,3 +7,8 @@ class Especialidad(models.Model):
     def __str__(self):
         return self.nombre
 
+class Habilidad(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
